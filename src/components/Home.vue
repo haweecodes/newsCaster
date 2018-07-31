e9d677e1ac55953b21d811db25720c5e3803e147
Sajib Hawee <haweesajib@yahoo.com>
2018-07-31 20:30:49 +0600

    changes to dashboard
    fixed dashboard color,
    added navigation (all),
    search enter fixed

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,7 +1,15 @@
 <template>
   <v-app>
-    <v-navigation-drawer clipped fixed v-model="drawer" app  class="blue lighten-3" dark >
+    <v-navigation-drawer clipped fixed v-model="drawer" app dark >
                <v-list>
+                 <v-list-tile @click="callAll">
+                <v-list-tile-action>
+                      <v-icon>language</v-icon>
+                    </v-list-tile-action>
+                    <v-list-tile-content>
+                      <v-list-tile-title>All</v-list-tile-title>
+                    </v-list-tile-content>
+            </v-list-tile>
             <v-list-tile @click="callCategory('sports')">
                 <v-list-tile-action>
                       <v-icon>directions_bike</v-icon>
@@ -20,7 +28,7 @@
             </v-list-tile>
           </v-list>>
       </v-navigation-drawer>
-      <v-toolbar app fixed clipped-left class="blue lighten-3" dark>
+      <v-toolbar app fixed clipped-left dark>
         <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
         <v-toolbar-title>NewsCaster</v-toolbar-title>
         <v-spacer></v-spacer>
@@ -28,7 +36,7 @@
            <v-spacer></v-spacer>
            <v-layout>
              <v-flex xs12>
-            <v-text-field label="Search News" append-icon="search" v-model="search" placeholder="Enter To Search" @keyup.enter="searchEntered(search)"></v-text-field>
+            <v-text-field label="Search News" :rules="searchRules" append-icon="search" v-model="search" placeholder="Press Enter To Search" @keyup.enter="searchEntered(search)"></v-text-field>
              </v-flex>
            </v-layout>
             <!-- <v-menu>
@@ -47,9 +55,14 @@
 
       <v-content>
       <v-container fluid grid-list-xs grid-list-sm grid-list-lg grid-list-md align-center>
+        <v-layout v-if="isSearch">
+          <v-flex xs12>
+              <label for="">Searched for <i>{{search}}</i></label>
+          </v-flex>
+        </v-layout>
         <v-layout row wrap v-if="searched === false">
-          <v-flex xs12 lg4 v-for="(item, i) in articles" :key=i>
-             <v-card>
+          <v-flex xs12 lg4 v-for="(item, i) in articles" :key=i >
+             <v-card class="fullHeight">
               <v-card-media
                 :src="item.urlToImage"
                 height="200px"
@@ -61,10 +74,9 @@
                   <div>{{item.description}} - {{item.source.name}}</div>
                 </div>
               </v-card-title>
-
-              <v-card-actions text-align="right">
+                  <v-card-actions text-align="right">
                 <v-spacer></v-spacer>
-                <v-btn flat color="orange" @click="window.location = item.url">View</v-btn>
+                <v-btn flat color="orange" @click="item.url">View</v-btn>
                 <v-btn flat color="orange">Share</v-btn>
               </v-card-actions>
             </v-card>
@@ -89,70 +101,103 @@
 
 <script>
 export default {
-  name: 'home',
-  data () {
+  name: "home",
+  data() {
     return {
       articles: [],
       country: [],
-      search: '',
+      search: "",
       searched: false,
-      drawer: true
-    }
+      drawer: true,
+      isSearch: false,
+      searchRules: [
+          v => !!v || 'Search cannot be empty'
+      ]
+    };
   },
-  beforeCreate () {
-    let vue = this
-    this.$store.dispatch('getNews').then(function (response) {
-      vue.articles = response.articles
+  beforeCreate() {
+    let vue = this;
+    this.$store.dispatch("getNews").then(function(response) {
+      vue.articles = response.articles;
       vue.articles.forEach(element => {
-        if(element.urlToImage === null) {
-          element.urlToImage = 'https://vignette.wikia.nocookie.net/simpsons/images/6/60/No_Image_Available.png/revision/20170219125728'
+        if (element.urlToImage === null) {
+          element.urlToImage =
+            "https://vignette.wikia.nocookie.net/simpsons/images/6/60/No_Image_Available.png/revision/20170219125728";
         }
-      })
-    })
+      });
+    });
   },
-  methods:{
-    searchEntered (keyword) {
-      let vue = this
-      this.searched = true
-        this.$store.dispatch('getAllNews', keyword).then(function(response){
-          vue.articles = response.articles
-            vue.articles.forEach(element => {
-              if(element.urlToImage === null) {
-                element.urlToImage = 'https://vignette.wikia.nocookie.net/simpsons/images/6/60/No_Image_Available.png/revision/20170219125728'
-              }
-            })
-          vue.searched = false
-          vue.search = ''
-        })
-    },
-    callCategory (keyword) {
-      let vue = this
-          this.searched = true
-      this.$store.dispatch('getCategory', keyword).then(function (response){
-        vue.articles = response.articles
+  methods: {
+    searchEntered(keyword) {
+      
+      let vue = this;
+      
+
+      if (keyword === "") {
+        return this.searchRules = 'Search cannot be empty'
+      } else {
+        this.searched = true;
+             this.isSearch = true;
+        this.$store.dispatch("getAllNews", keyword).then(function(response) {
+          vue.articles = response.articles;
           vue.articles.forEach(element => {
-            if(element.urlToImage === null) {
-              element.urlToImage = 'https://vignette.wikia.nocookie.net/simpsons/images/6/60/No_Image_Available.png/revision/20170219125728'
+            if (element.urlToImage === null) {
+              element.urlToImage =
+                "https://vignette.wikia.nocookie.net/simpsons/images/6/60/No_Image_Available.png/revision/20170219125728";
             }
-          })
-        vue.searched = false
-      })
+          });
+          vue.searched = false;
+        });
+      }
+    },
+    callCategory(keyword) {
+      this.search = "";
+      this.isSearch = false;
+      let vue = this;
+      this.searched = true;
+      this.$store.dispatch("getCategory", keyword).then(function(response) {
+        vue.articles = response.articles;
+        vue.articles.forEach(element => {
+          if (element.urlToImage === null) {
+            element.urlToImage =
+              "https://vignette.wikia.nocookie.net/simpsons/images/6/60/No_Image_Available.png/revision/20170219125728";
+          }
+        });
+        vue.searched = false;
+      });
+    },
+    callAll() {
+      this.search = "";
+      this.isSearch = false;
+      let vue = this;
+      this.$store.dispatch("getNews").then(function(response) {
+        vue.articles = response.articles;
+        vue.articles.forEach(element => {
+          if (element.urlToImage === null) {
+            element.urlToImage =
+              "https://vignette.wikia.nocookie.net/simpsons/images/6/60/No_Image_Available.png/revision/20170219125728";
+          }
+        });
+      });
     }
   }
-}
+};
 </script>
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-.toolbarIcon{
+.toolbarIcon {
 }
-.headlineDecoration{
+.headlineDecoration {
   text-decoration: none;
-  color: black
+  color: black;
 }
-.headlineDecoration:hover{
-  color: rgb(255, 0, 55)
+.headlineDecoration:hover {
+  color: rgb(255, 0, 55);
 }
-.progress{
+.progress {
   text-align: center;
+}
+.fullHeight {
+  height: 100% !important;
 }
 </style>
